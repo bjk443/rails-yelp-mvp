1e5f18d20352a9993f34879f89cb9221b0d8acb0
john kang <johnkang420@gmail.com>
2020-06-24 13:40:38 +0800

    fixed model so that rating can only be integer

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,5 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   # numericality check
   RATINGS = [0,1,2,3,4,5]
-  validates :rating, inclusion: { in: RATINGS }
+  validates :rating, numericality: {only_integer: true }, inclusion: { in: RATINGS }
 end
